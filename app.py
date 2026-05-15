36034685bd6853239b225db407f9491a917fbbf7
NariNEzumi <146164972+NariNEzumi@users.noreply.github.com>
2026-05-15 11:30:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -182,3 +182,8 @@ def animal_page(animal_id):
 def animal_page(animal_id):
     animal = animals_data.get(animal_id)
     return render_template("animal.html", animal=animal)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
